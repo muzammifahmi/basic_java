34c40ac755db01414016ed6a016c1388963997f2
muzammifahmi <muzammifahmi1@gmail.com>
2025-03-14 13:33:30 +0700

    modif

---
+++ DataTypesExample.java
@@ -0,0 +1,13 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        int items = 100;
+        float costPerItem = 1.25f;
+        float totalCost = items * costPerItem;
+        char currency = '$';
+        //print
+        System.out.println("PROGRAM DATA BARANG");
+        System.out.println("Jumlah Barang : " + items);
+        System.out.println("Harga tiap Barang : " + costPerItem + currency);
+        System.out.println("Total Harga : " + totalCost + currency);
+    }
+}
--- variable.java
+++ variable.java
@@ -2,7 +2,7 @@ public class variable {
     public static void main(String[] args) {
         //display variable
         String name = "Fahmi";
-        System.out.println("Nama : " + name);
+        System.out.println("Nama : " + name + "\n");
 
         String firstName = "Achmad ";
         String lastName = "Muzammi Fahmi";
@@ -10,7 +10,7 @@ public class variable {
         int studentAge = 20;
         float studentFee = 1000.50f;
         char studentGrade = 'A';
-
+        System.out.println("PROGRAM DATA MAHASISWA");
         System.out.println("Nama Lengkap : " + firstName + lastName);
         System.out.println("NIM          : " + studentID);
         System.out.println("Umur         : " + studentAge);
@@ -28,5 +28,14 @@ public class variable {
         System.out.println("hasil penjumlahan dari " + angkaPertama + " + " + angkaKedua + " + " + angkaKetiga + " adalah : " + (angkaPertama + angkaKedua + angkaKetiga));
         System.out.println("hasil pengurangan dari " + angkaPertama + " - " + angkaKedua + " - " + angkaKetiga + " adalah : " + (angkaPertama - angkaKedua - angkaKetiga) + "\n"); 
 
+        //penerapan variable untuk membuat program luas persegi panjang
+        int length = 10, width = 5, area;
+        //rumus luas persegi panjang
+        area = length * width;
+        //print hasil luas persegi panjang
+        System.out.println("PROGRAM LUAS PERSEGI PANJANG");
+        System.out.println("Panjang              : " + length);
+        System.out.println("Lebar                : " + width);
+        System.out.println("Luas persegi panjang : " + area + "\n");
     }
 }
