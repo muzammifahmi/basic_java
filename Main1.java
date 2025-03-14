c3d29d834b9393b62d2669cde47807060b151b5f
muzammifahmi <muzammifahmi1@gmail.com>
2025-03-14 09:21:35 +0700

    first push

---
+++ Main1.java
@@ -0,0 +1,35 @@
+public class Main1{
+  public static void main(String[] args){
+    // kode dibawah contoh program sederhana
+    System.out.println("hello world"); // prints hello world
+    System.out.println(1000); // prints 1000
+    System.out.println(3.14); // prints 3.14
+    System.out.println(2 + 4); // prints 6 (2 + 4 = 6)
+    System.out.println(10 - 5); // prints 5 (10 - 5 = 5)
+    System.out.println(3 * 5); // prints 15 (3 * 5 = 15)
+    System.out.println(10 / 2); // prints 5 (10 / 2 = 5)
+    System.out.println(10 % 3); // prints 1 (10 % 3 = 1)
+    System.out.println(4 == 40); // prints false (4 is not equal to 40)
+    System.out.println(4 != 40); // prints true (4 is not equal to 40)
+
+    // kode dengan tipe data string
+    String name = "Achmad Muzammi Fahmi";
+    System.out.println(name);
+
+    // kode dengan tipe data integer
+    int umur = 20;
+    System.out.println(umur);
+
+    //kode dengan tipe data float
+    float tinggi = 170.5f;
+    System.out.println(tinggi);
+
+    // kode dengan tipe data char
+    char JenisKelamin = 'L';
+    System.out.println(JenisKelamin);
+
+    //kode dengan tipe data boolean
+    boolean StatusNikah = false;
+    System.out.println(StatusNikah);
+   }
+}
