a40ea42f50e8e0a63bfb8551a8e338354be07961
muzammifahmi <muzammifahmi1@gmail.com>
2025-03-15 17:33:43 +0700

    push

---
+++ AssignmentOperator.java
@@ -0,0 +1,51 @@
+public class AssignmentOperator {
+    public static void main(String[] args) { 
+         //Java Assignment Operator
+         System.out.println("IMPLEMENTASI ASSIGNMENT OPERATOR");
+         System.out.println("=====================================");
+         System.out.println("NILAI AWAL : 10");
+         //1. operator "="
+         int number1 = 10;
+         System.out.println(number1);
+         //2. operator "+="
+         int number2 = 10;
+         number2 += 5;
+         System.out.println(number2);
+         //3. operator "-="
+         int number3 = 10;
+         number3 -= 5;
+         System.out.println(number3);
+         //4. operator "*="
+         int number4 = 10;
+         number4 *= 5;
+         System.out.println(number4);
+         //5. operator "/="
+         int number5 = 10;
+         number5 /= 5;
+         System.out.println(number5);
+         //6. operator "%="
+         int number6 = 10;
+         number6 %= 5;
+         System.out.println(number6);
+         //7. operator "&="
+         int number7 = 10;
+         number7 &= 5;
+         System.out.println(number7);
+         //8. operator "|="
+         int number8 = 10;
+         number8 |= 5;
+         System.out.println(number8);
+         //9. operator "^="
+         int number9 = 10;
+         number9 ^= 5;
+         System.out.println(number9);
+         //10. operator ">>="
+         int number10 = 10;
+         number10 >>= 5;
+         System.out.println(number10);
+         //11. operator "<<="
+         int number11 = 10;
+         number11 <<= 5;
+         System.out.println(number11);
+    }
+}
